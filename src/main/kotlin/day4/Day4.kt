64d89f829bc28d2cd094531cc01602bf66c53045
Nico Thomas <thomasnico@web.de>
2024-12-04 18:53:36 +0100

    Add Day 4

---
+++ src/main/kotlin/day4/Day4.kt
@@ -0,0 +1,49 @@
+package day4
+
+import readInput
+
+val offsets1 = listOf(
+    listOf(Pair(0,0), Pair(-1,0), Pair(-2,0), Pair(-3,0)),
+    listOf(Pair(0,0), Pair(1,0), Pair(2,0), Pair(3,0)),
+    listOf(Pair(0,0), Pair(0,-1), Pair(0,-2), Pair(0,-3)),
+    listOf(Pair(0,0), Pair(0,1), Pair(0,2), Pair(0,3)),
+    listOf(Pair(0,0), Pair(-1,-1), Pair(-2,-2), Pair(-3,-3)),
+    listOf(Pair(0,0), Pair(1,-1), Pair(2,-2), Pair(3,-3)),
+    listOf(Pair(0,0), Pair(-1,1), Pair(-2,2), Pair(-3,3)),
+    listOf(Pair(0,0), Pair(1,1), Pair(2,2), Pair(3,3))
+)
+
+val offsets2 = listOf(
+    listOf(Pair(0,0), Pair(-1,-1), Pair(1,1), Pair(-1,1), Pair(1,-1)),
+    listOf(Pair(0,0), Pair(1,1), Pair(-1,-1), Pair(1,-1), Pair(-1,1)),
+    listOf(Pair(0,0), Pair(-1,-1), Pair(1,1), Pair(1,-1), Pair(-1,1)),
+    listOf(Pair(0,0), Pair(1,1), Pair(-1,-1), Pair(-1,1), Pair(1,-1)),
+)
+
+fun  List<String>.findMatches(word: String, offsets: List<List<Pair<Int, Int>>>): Int {
+    var matches = 0
+
+    for (y in this.indices) {
+        for (x in this[y].indices) {
+            direction@ for (offsetList in offsets) {
+                for (offsetIndex in offsetList.indices) {
+                    val xOff = x + offsetList[offsetIndex].first
+                    val yOff = y + offsetList[offsetIndex].second
+                    if (xOff < 0 || xOff >= this[y].length || yOff < 0 || yOff >= this.size) continue@direction
+                    val char = this[yOff][xOff]
+                    if (char != word[offsetIndex]) continue@direction
+                    if (offsetIndex == offsetList.size-1) matches++
+                }
+            }
+        }
+    }
+
+    return matches
+}
+
+fun main() {
+    val input = readInput(4, false).lines()
+
+    println(input.findMatches("XMAS", offsets1))
+    println(input.findMatches("AMSMS", offsets2))
+}
