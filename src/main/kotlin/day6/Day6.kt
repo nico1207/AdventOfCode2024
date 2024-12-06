18ebbee9cda579053492659e903ff40fedd32194
Nico Thomas <thomasnico@web.de>
2024-12-06 15:25:05 +0100

    Optimize

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -2,7 +2,7 @@ package day6
 
 import readInput
 
-fun List<String>.walkPath(startPos: Vec): Int {
+fun List<String>.walkPath(startPos: Vec): Set<Vec> {
     var pos = startPos
     var dir = Vec(0, -1)
     val visited = mutableSetOf(pos)
@@ -19,12 +19,12 @@ fun List<String>.walkPath(startPos: Vec): Int {
             visited += pos
         }
         if (pastStates.contains(pos to dir)) {
-            return -1
+            return setOf()
         }
         pastStates += pos to dir
     }
 
-    return visited.size
+    return visited
 }
 
 fun main() {
@@ -33,19 +33,17 @@ fun main() {
     var guardPositionX = input[guardPositionY].indexOf("^")
     val guardPos = Vec(guardPositionX, guardPositionY)
 
-    println(input.walkPath(guardPos))
+    val pathPositions = input.walkPath(guardPos)
+    println(pathPositions.size)
 
-    // Bruteforce loop obstructions
-    // Could be optimized by saving the path from Part 1 and trying all positions along this path
+    // Try all positions along path, if they cause a loop
     var obstructions = 0
-    for (y in input.indices) {
-        for (x in input[y].indices) {
-            val char = input[y][x]
-            if (char != '.') continue
-            val inputCopy = input.toMutableList()
-            inputCopy[y] = StringBuilder(inputCopy[y]).also { it.setCharAt(x, '#') }.toString()
-            if (inputCopy.walkPath(guardPos) == -1) obstructions++
-        }
+    for (pos in pathPositions) {
+        val char = input[pos.y][pos.x]
+        if (char != '.') continue
+        val inputCopy = input.toMutableList()
+        inputCopy[pos.y] = StringBuilder(inputCopy[pos.y]).also { it.setCharAt(pos.x, '#') }.toString()
+        if (inputCopy.walkPath(guardPos).isEmpty()) obstructions++
     }
 
     println(obstructions)
