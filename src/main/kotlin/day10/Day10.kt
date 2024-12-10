03b9be1bb338c1e358af37b04634219c98ae3391
Nico Thomas <thomasnico@web.de>
2024-12-10 19:14:30 +0100

    Add Day 10

---
+++ src/main/kotlin/day10/Day10.kt
@@ -0,0 +1,37 @@
+package day10
+
+import Vec
+import for2D
+import readInput
+
+val offsets = listOf(
+    Vec(-1, 0), Vec(1, 0),  Vec(0, -1),  Vec(0, 1)
+)
+
+fun main() {
+    val input = readInput(10, false).lines()
+
+    val trailheads = mutableSetOf<Vec>()
+    for2D(input) { x, y ->
+        if (input[y][x] == '0') trailheads.add(Vec(x, y))
+    }
+
+    val part1 = trailheads.sumOf { th ->
+        val reachablePositions = mutableMapOf('0' to mutableSetOf(th))
+        for (i in '0'..'8') {
+            val curr = reachablePositions[i] ?: continue
+            reachablePositions[i+1] = mutableSetOf()
+            for (pos in curr) {
+                for (off in offsets) {
+                    val offPos = pos + off
+                    if (offPos liesIn input && input[offPos.y][offPos.x] == i+1) {
+                        reachablePositions[i+1]!!.add(offPos)
+                    }
+                }
+            }
+        }
+        reachablePositions['9']?.size ?: 0
+    }
+
+    println(part1)
+}
---
+++ src/main/kotlin/day10/Day10_2.kt
@@ -0,0 +1,34 @@
+package day10
+
+import Vec
+import for2D
+import readInput
+
+fun main() {
+    val input = readInput(10, true).lines()
+
+    val trailheads = mutableSetOf<Vec>()
+    for2D(input) { x, y ->
+        if (input[y][x] == '0') trailheads.add(Vec(x, y))
+    }
+
+    val part2 = trailheads.sumOf { th ->
+        getRating(th, input)
+    }
+
+    println(part2)
+}
+
+fun getRating(pos: Vec, input: List<String>): Int {
+    val currVal = input[pos.y][pos.x]
+    if (currVal == '9') return 1
+
+    return offsets.sumOf {
+        val offPos = pos + it
+        if (offPos liesIn input && input[offPos.y][offPos.x] == currVal + 1) {
+            return@sumOf getRating(offPos, input)
+        } else {
+            return@sumOf 0
+        }
+    }
+}
