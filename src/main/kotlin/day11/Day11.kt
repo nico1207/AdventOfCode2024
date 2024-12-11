c99a6041f889b9f8ce76b6ad8c4a1f0a0ed684fe
Nico Thomas <thomasnico@web.de>
2024-12-11 19:17:36 +0100

    Make nicer

--- src/main/kotlin/day11/Day11.kt
+++ src/main/kotlin/day11/Day11.kt
@@ -5,27 +5,26 @@ import readInput
 fun main() {
     val input = readInput(11, false).split(" ").map { it.toLong() }
 
-    var map = input.associateWith { 1L }
+    var stoneCounts = input.associateWith { 1L }
 
-    for (i in 0..<75) {
-        val newMap = mutableMapOf<Long, Long>()
-        for ((n, v) in map) {
-            val nStr = n.toString()
-            if (n == 0L) {
-                newMap[1] = newMap.getOrDefault(1, 0) + v
-                continue
+    repeat(75) {
+        val newCounts = mutableMapOf<Long, Long>()
+        stoneCounts.forEach { (number, count) ->
+            when {
+                number == 0L -> newCounts.merge(1L, count, Long::plus)
+                number.toString().length % 2 == 0 -> {
+                    val numStr = number.toString()
+                    val firstHalf = numStr.take(numStr.length / 2).toLong()
+                    val secondHalf = numStr.takeLast(numStr.length / 2).toLong()
+
+                    newCounts.merge(firstHalf, count, Long::plus)
+                    newCounts.merge(secondHalf, count, Long::plus)
+                }
+                else -> newCounts.merge(number * 2024, count, Long::plus)
             }
-            if (nStr.length % 2 == 0) {
-                val a = nStr.take(nStr.length / 2).toLong()
-                val b = nStr.takeLast(nStr.length / 2).toLong()
-                newMap[a] = newMap.getOrDefault(a, 0L) + v
-                newMap[b] = newMap.getOrDefault(b, 0L) + v
-                continue
-            }
-            newMap[n * 2024L] = newMap.getOrDefault(n * 2024, 0) + v
         }
-        map = newMap
+        stoneCounts = newCounts
     }
 
-    println(map.values.sum())
+    println(stoneCounts.values.sum())
 }
